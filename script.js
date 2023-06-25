a2758a6838034c52f54e9eb088f7b3a7114274d4
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-06-25 20:36:57 +1000

    added event listeners

--- script.js
+++ script.js
@@ -22,23 +22,35 @@ $(function () {  //shorthand for $(document).ready(function()
 
 //Function that will apply past,present and future classes to each time block   
   function checkTime() {
-    var getTime = dayjs(). hour(); //use dayjs to get current time
+    var getTime = dayjs().format ("H"); //use dayjs to get current time
     $(".time-block").each(function () { //uses .each() method to iterate over each class time-block variable
     var timeBlock = $(this).attr("id"); //retrieves the ID of the current time and assigns it to the timeBlock variable. 
-    var timeBlockHours = parseInt($(this).attr("id").split("-")[1]); // extracts the hour by splitting the the ID string and prasing it as an integer 
+    var timeGetHours = parseInt($(this).attr("id").split("-")[1]); // extracts the hour by splitting the the ID string and prasing it as an integer 
     var descriptionId = localStorage.getItem(timeBlock);
+
+    console.log("Current Time: " + getTime);
+    console.log("Description: " + descriptionId);
 
     if (descriptionId) {
       $(this).find(".description").val(descriptionId);
     }
-    if (timeBlockHours < getTime) {
-      $(this).addClass("past");
-    } else if (timeBlockHours == getTime) {
-      $(this).addClass("present");
+    
+    if (timeGetHours < getTime) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (timeGetHours > getTime) {
+      $(this).removeClass("past present").addClass("future");
     } else {
-      $(this).addClass("future");
+      $(this).removeClass("past future").addClass("present");
     }
-    });
+    
+    // Additional if statement for 8 PM
+    if (timeGetHours === 20) {
+      $(this).removeClass("past present").addClass("future");
+    }
+      
+    
+});
+    
   }
 
   checkTime();
